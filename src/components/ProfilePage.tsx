fe45b0971db7ff99e35ec012f8c9cddb4ce19d7f
Nastia Khalus <nastiakhalus@gmail.com>
2024-06-04 16:54:02 +0300

    added changes to profilepage

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -1,12 +1,67 @@
+import React from 'react';
 import './ProfilePage.css';
+import logo from '../assets/BEHONEST02.png'; // Adjust the path as necessary
+import profileIcon from '../assets/profile_icon.png'; // Adjust the path as necessary
 
 const ProfilePage = () => {
     return (
-        <div>
+        <div className="app">
+            <Header />
+            <SearchBar />
             <div className="profile-header">Hello</div>
             <div className="white-rectangle">Bye</div>
         </div>
     );
 };
 
+function Header() {
+    return (
+        <header className="header">
+            <img src={logo} alt="Logo" className="logo" />
+            <img src={profileIcon} alt="Profile" className="profile-icon" />
+        </header>
+    );
+}
+
+function SearchBar() {
+    return (
+        <div className="search-bar">
+            <input type="text" placeholder="Search your friends" className="search-input" />
+        </div>
+    );
+}
+
 export default ProfilePage;
+
+
+
+
+
+
+
+
+// function MainContent() {
+//     return (
+//         <main className="main-content">
+//             <h1 className="main-title">QUESTION FOR YOU</h1>
+//             <FriendResponse nickname="nickname" answer="friend’s answer" />
+//             <FriendResponse nickname="nickname" answer="friend’s answer" />
+//             <FriendResponse nickname="nickname" answer="friend’s answer" />
+//             <FriendResponse nickname="nickname" answer="friend’s answer" />
+//         </main>
+//     );
+// }
+
+// function FriendResponse({ nickname, answer }) {
+//     return (
+//         <div className="friend-response">
+//             <div className="friend-avatar"></div>
+//             <div className="friend-details">
+//                 <div className="friend-nickname">{nickname}</div>
+//                 <div className="friend-answer">{answer}</div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Scrolling;
