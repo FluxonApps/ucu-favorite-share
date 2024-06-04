28919f7013faae86e1e842314256ed144488eab2
sofiasampara76 <149332388+sofiasampara76@users.noreply.github.com>
2024-06-04 17:31:40 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { App } from './App.js';
+import { App } from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
