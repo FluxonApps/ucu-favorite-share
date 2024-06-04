6c4c076df6742a96a19a8797a9c98cb8818baf1e
Nastia Khalus <nastiakhalus@gmail.com>
2024-06-04 15:21:31 +0300

    created new branch

---
+++ src/components/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import './ProfilePage.css';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <div className="profile-header">Hello</div>
+            <div className="white-rectangle">Bye</div>
+        </div>
+    );
+};
+
+export default ProfilePage;
