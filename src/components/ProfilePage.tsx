24361ec5bf6ecf234a7f93c4886d753fbd59e489
Nastia Khalus <nastiakhalus@gmail.com>
2024-06-05 16:11:29 +0300

    button

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -66,6 +66,7 @@ const ProfilePage = () => {
           <div>
             <div className="nickname">{currentUser.username}</div>
             <div className="followers">{currentUser.followers.length} Followers</div>
+            <div className="button">Change</div>
           </div>
         </div>
         <div className="cards row">
@@ -110,28 +111,3 @@ function Card({ question, answer }) {
 
 export default ProfilePage;
 
-// function MainContent() {
-//     return (
-//         <main className="main-content">
-//             <h1 className="main-title">QUESTION FOR YOU</h1>
-//             <FriendResponse nickname="nickname" answer="friend’s answer" />
-//             <FriendResponse nickname="nickname" answer="friend’s answer" />
-//             <FriendResponse nickname="nickname" answer="friend’s answer" />
-//             <FriendResponse nickname="nickname" answer="friend’s answer" />
-//         </main>
-//     );
-// }
-
-// function FriendResponse({ nickname, answer }) {
-//     return (
-//         <div className="friend-response">
-//             <div className="friend-avatar"></div>
-//             <div className="friend-details">
-//                 <div className="friend-nickname">{nickname}</div>
-//                 <div className="friend-answer">{answer}</div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Scrolling;
